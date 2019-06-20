6f157cb2c017e15c6077d761bbb67f6e50761c27
MoshniyVasiya <vasyashapran93@gmail.com>
2019-06-21 01:23:17 +0300

    with_some_2nd_part

--- lesson_10/index.js
+++ lesson_10/index.js
@@ -7,6 +7,10 @@ const buy = document.getElementById('buy');
 const calcButton = document.getElementById('calc_button');
 let select1 = document.getElementById('select1');
 let select2 = document.getElementById('select2');
+let container = document.getElementById('container');
+let usdString = document.getElementById('usdString');
+let eurString = document.getElementById('eurString');
+let gbpString = document.getElementById('gbpString');
 
 
 // converter
@@ -43,6 +47,21 @@ showButton.addEventListener('click', () => {
     let toResult = to.value;
     console.log(fromResult, toResult);
     
+
+
+    
+    fetch(`https://api.exchangeratesapi.io/history?start_at=${fromResult}&end_at=${toResult}`)
+    .then(response => response.json())
+    .then(intervals => { 
+        console.log(intervals)
+        for(let key in intervals.rates){  
+            console.log(key)
+
+            usdString.innerText = `USD: ${intervals.rates[key].USD}`
+            gbpString.innerText = `GBP: ${intervals.rates[key].GBP}`
+            eurString.innerText = `EUR: ${intervals.rates[key].EUR}`
+
+        }
+    });
 });
 
-fetch('https://api.exchangeratesapi.io/2010-01-12');
