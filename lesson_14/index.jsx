75c27563f552c8bbfb617136698dc55ad7978d11
MoshniyVasiya <vasyashapran93@gmail.com>
2019-07-09 12:17:12 +0300

    emojiwrapper_done

--- lesson_14/index.jsx
+++ lesson_14/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {WrapInEmoji} from './wrapinemoji';
+import WrapInEmoji from './wrapinemoji';
 
 
 ReactDOM.render((   
 
 <div>   
-    <WrapInEmoji >some text</WrapInEmoji>
+    <WrapInEmoji defaultEmoji="128545">some text</WrapInEmoji>
     
 </div>
 ), document.getElementById('root'));
--- lesson_14/wrapinemoji.jsx
+++ lesson_14/wrapinemoji.jsx
@@ -1,31 +1,53 @@
 import React from 'react';
 
-const emojis = ['U+1F600', 'U+1F603', 'U+1F604', 'U+1F601', 'U+1F606'];
+const utf = 'U+1F'
 
-export class WrapInEmoji extends React.Component { 
-    constructor() { 
-        super();
-        
-        
-        
-        this.onEmojiClick = this.onEmojiClick.bind(this);
-        
-    }
+export default class WrapInEmoji extends React.Component{
+	constructor(){
+		super();
 
-onEmojiClick() {    
-    this.setState({ 
+		
+		this.state = {
+			left: String.fromCodePoint(Math.floor(Math.random() * (128586 - 128513)) + 128513),
+			right: String.fromCodePoint(Math.floor(Math.random() * (128586 - 128513)) + 128513),
+		};
 
-    })
-}
+		
+    	this.leftEmoji = this.leftEmoji.bind(this);
+		this.rightEmoji = this.rightEmoji.bind(this);
+		this.state.default = true;
+	}
 
-render() {  
-    const defaultEmoji = this.props.defaultEmoji;
-    return (
-        <div>   
-        {}
-        <span><br /></span>
-        {}
-        </div> 
-    )
-}
+	leftEmoji(){
+		this.setState({
+		left: this.state.left= String.fromCodePoint(Math.floor(Math.random() * (128586 - 128513)) + 128513)
+		});
+	}
+	rightEmoji(){
+		this.setState({
+		right: this.state.right = String.fromCodePoint(Math.floor(Math.random() * (128586 - 128513)) + 128513)
+		});
+	}
+
+	componentDidMount() {	
+		this.state.default = false;
+		
+	}
+	
+	render(){
+		if(this.props.defaultEmoji && this.state.default){	
+			this.state = {	
+				left: String.fromCodePoint(this.props.defaultEmoji) ,
+				right: String.fromCodePoint(this.props.defaultEmoji) ,
+			}
+		}
+		
+		return(
+		<div>
+			<span onClick={this.leftEmoji}>{this.state.left}</span>
+				{this.props.children}
+			<span onClick={this.rightEmoji}>{this.state.right}</span>
+		</div>
+		)
+	}
 }
